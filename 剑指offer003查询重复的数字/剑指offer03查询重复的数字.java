64107a616a846a93619bf695cfb7189254420c9a
zhang-jia-yu <2533141110@qq.com>
2022-01-27 16:08:56 +0800

    Update 剑指offer03查询重复的数字.java

--- 剑指offer003查询重复的数字/剑指offer03查询重复的数字.java
+++ 剑指offer003查询重复的数字/剑指offer03查询重复的数字.java
@@ -3,7 +3,9 @@ class Solution {
         int len = nums.length;
         Set<Integer> dic = new HashSet<Integer>();
         for (int num : nums ) {
+        //int num : nums 循环输出数组元素
           if(dic.contains(num)){
+        //判断dic是否包含num
               return num;
           }
           dic.add(num);
