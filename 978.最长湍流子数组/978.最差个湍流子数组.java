fcc6181d79cfc6fb64adfc644889913a1e30f437
zhang-jia-yu <2533141110@qq.com>
2022-01-29 13:26:25 +0800

    Create 978.最差个湍流子数组.java

---
+++ 978.最长湍流子数组/978.最差个湍流子数组.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxTurbulenceSize(int[] arr) {
+        int len = arr.length;
+        int[] up = new int[len] , down = new int[len];
+        Arrays.fill(up , 1);
+        Arrays.fill(down , 1);
+        int ans = 1;
+        for (int i = 1; i < len; i++) {
+            if (arr[i] > arr[i - 1]){
+                up[i] = down[i - 1] + 1;
+                ans = Math.max(ans , up[i]);
+            }else if (arr[i] < arr[i - 1]){
+                down[i] = up[i - 1] + 1;
+                ans= Math.max(ans , down[i]);
+            }
+        }
+        return ans;
+    }
+}
+
+解法：  https://leetcode-cn.com/problems/longest-turbulent-subarray/solution/yi-zhang-dong-tu-xiang-jie-dong-tai-gui-wrwvn/
