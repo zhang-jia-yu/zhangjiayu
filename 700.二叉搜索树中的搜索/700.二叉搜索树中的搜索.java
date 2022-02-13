6d1c379f17ecfe742dabf7cefd88a5a4d022b6be
zhang-jia-yu <2533141110@qq.com>
2022-02-13 17:25:11 +0800

    Update 700.二叉搜索树中的搜索.java

--- 700.二叉搜索树中的搜索/700.二叉搜索树中的搜索.java
+++ 700.二叉搜索树中的搜索/700.二叉搜索树中的搜索.java
@@ -7,7 +7,7 @@ class Solution {
             if(root.val == val){
                 return root;
             }
-            root = val < root.val ? root.left : root.right;
+            root = val < root.val ? root.left : root.right;   //A ？ B ：C (如果A为真执行B否则执行C)
         }
         return root;
     }
