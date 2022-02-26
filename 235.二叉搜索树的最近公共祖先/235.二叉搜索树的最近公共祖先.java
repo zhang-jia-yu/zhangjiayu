02f40a10d101aceef9681574bc1031490101368f
zhang-jia-yu <2533141110@qq.com>
2022-02-26 15:27:16 +0800

    Create 235.二叉搜索树的最近公共祖先.java

---
+++ 235.二叉搜索树的最近公共祖先/235.二叉搜索树的最近公共祖先.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode node = root;
+        while(true){
+            if(node.val > p.val && node.val > q.val){
+                node = node.left;
+            }else if(node.val < p.val && node.val < q.val){
+                node = node.right;
+            }else{
+                break; 
+            }
+ 
+        }
+        return node;
+        
+    }
+}
